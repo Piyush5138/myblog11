a6bf03d1f126837b19ebfa5ee2c743a6349f5ddf
piyush <piyush.ra.one@gmail.com>
2024-02-25 23:19:36 +0530

    127:Create save Post Features

---
+++ src/test/java/myblog11/com/myblog11/ApplicationTests.java
@@ -0,0 +1,13 @@
+package myblog11.com.myblog11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
